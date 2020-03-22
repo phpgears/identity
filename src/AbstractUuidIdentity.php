c40dbbb5df71db0d50b12cf832655d93f63d1bdb
Julián Gutiérrez <juliangut@gmail.com>
2020-03-22 22:37:14 +0100

    allow ramsey's uuid ^4.0

--- src/AbstractUuidIdentity.php
+++ src/AbstractUuidIdentity.php
@@ -15,6 +15,7 @@ namespace Gears\Identity;
 
 use Gears\Identity\Exception\InvalidIdentityException;
 use Ramsey\Uuid\Exception\InvalidUuidStringException;
+use Ramsey\Uuid\Nonstandard\Uuid as NonstandardUuid;
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
 
@@ -56,7 +57,9 @@ abstract class AbstractUuidIdentity extends AbstractIdentity
      */
     final protected static function assertUuidVariant(UuidInterface $uuid): void
     {
-        if ($uuid->getVariant() !== Uuid::RFC_4122 || !\in_array($uuid->getVersion(), \range(1, 5), true)) {
+        if ((\class_exists('Ramsey\Uuid\Nonstandard\Uuid') && $uuid instanceof NonstandardUuid)
+            || ($uuid->getVariant() !== Uuid::RFC_4122 || !\in_array($uuid->getVersion(), \range(1, 5), true))
+        ) {
             throw new InvalidIdentityException(
                 \sprintf('Provided identity value "%s" is not a valid UUID', $uuid->toString())
             );
